4784902447cd445772fb1e6550c5787e8fe8a312
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-01-24 18:42:17 +0530

    remove persons model migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_24_103631) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_24_123939) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -28,5 +28,3 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_24_103631) do
     t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
-
- 
