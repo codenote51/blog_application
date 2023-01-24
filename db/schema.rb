9353f2df0e866f91035c68101c005b7b0f4e33bd
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-01-24 18:00:29 +0530

    remove migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_23_125157) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_24_103631) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -29,5 +29,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_23_125157) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
-  add_foreign_key "comments", "articles"
-end
+ 
