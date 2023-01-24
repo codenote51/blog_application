b40cee9990e70411896c8d78c51ce40b9edb2d4f
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-01-24 18:39:33 +0530

    end tag remove

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/text'
+  get 'persons/simple'
   #get 'simple/text'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
