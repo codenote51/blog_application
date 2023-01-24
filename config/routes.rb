af62d719478881ec8cd89592a78d4f58b67bf038
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-01-24 17:58:08 +0530

    remove simple root, resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  #get 'simple/text'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
    root "articles#index"
     resources :articles do
-       resources :comments
+       resources :comments 
+      end 
     end
-end
