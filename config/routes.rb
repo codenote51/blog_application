68eb1dd558243997aa1ae74ccfdb85a2be85280e
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-01-30 14:13:27 +0530

    resources :orders

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "articles#index"
-    resources :articles do
-       resources :comments 
-      end 
-    end
+  root "articles#index"
+
+  resources :articles do
+    resources :comments 
+  end
+
+  resources :orders
+end
