4821662b95f8b63eee974e0ea5f9951044daea99
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-02-01 18:12:59 +0530

    remove has_many

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,35 @@
 class Order < ApplicationRecord
-
-  validates :name, presence: true
-  validates :lastname, presence: true
+  validates :name,  presence: true, length: { minimum: 3 }
+  validates :lastname, presence: true 
   validates :email, presence: true
+
+   #after_touch :log_when_book_or_order_touched
 end
+
+  #before_save :name,  if: Proc.new { name? }
+  #if: Proc.new { |order| orde.name? }
+
+   #before_create do 
+    #self.name = login.capitalize if name.blank?
+  #end
+
+   #before_validation :ensure_login_has_a_value
+  #unless: Proc.new { |a| a.email.blank? }
+  #unless: -> { email.blank? } 
+
+  #private
+  #def  ensure_login_has_a_value
+    #if login.nill?
+      #self.login = email unless email.blank?   
+   # end
+  #end
+
+  #after_initialize do |order|
+    #puts "You have initialized an object!"
+  #end
+
+  #after_find do |order|
+    #puts "You have found an object!"
+  #end
+
+
