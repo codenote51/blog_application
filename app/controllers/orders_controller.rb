2d876d966928e62575666a145ad4848cbbda5fa6
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-02-01 14:28:40 +0530

    remove new record

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,39 @@ class OrdersController < ApplicationController
   end
 
   def new
-    @order = Order.new(name: "...", lastname: "...", email: "...")
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.find(order_params)
+    if @order.save
+      redirect_to @order
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit 
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    if update.order(order_params)
+      redirect_to @order
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+    redirect_to root_path, status: :see_other
+  end 
+
+  private
+  def order_params
+    params.require(:order).permit(:title, :body)
   end
 end
