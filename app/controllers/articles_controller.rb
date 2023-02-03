e869a0b32dd5fdf0ac547fd8d0dfe2686a4daf78
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-02-03 16:12:38 +0530

    fix indentation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,4 @@
 class ArticlesController < ApplicationController
-  #http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
-  
   def index
     @articles = Article.all
   end
@@ -8,25 +6,23 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-  
+
   def new
     @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
+
     if @article.save
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
     end
   end
-  
+
   def edit
-    @article = Article.find_by(id: params[:id])
-    unless @article
-      redirect_to root_path #, status: :not_found
-    end
+    @article = Article.find(params[:id])
   end
 
   def update
@@ -42,11 +38,12 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+
     redirect_to root_path, status: :see_other
-  end 
+  end
 
   private
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :status)
     end
 end
