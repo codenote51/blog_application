6875e41f6f9de02f6836fc7cbaeb5a18c36be732
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-02-10 18:38:37 +0530

    remove customer action

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,12 @@
+class CustomersController < ApplicationController
+    def create
+      @order = Order.find(params[:order_id])
+      @customer = @order.customers.create(customer_params)
+      redirect_to order_path(@order)
+    end
+
+    private
+    def comment_params
+      params.require(:order).permit(:name, :lastname, :status)
+    end
+end
