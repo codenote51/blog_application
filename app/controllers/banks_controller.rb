fc4e91461484b754b54e42a9403bb321452cfabd
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-02-10 18:15:55 +0530

    remove layout

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,50 +1,44 @@
 class BanksController < ApplicationController
   layout :banks_layouts
-
   def index 
     @banks = Bank.all
   end
 
-
   def show
     @bank = Bank.find(params[:id])
-  end
+  end 
 
   def new
     @bank = Bank.new
   end
 
-
   def create
     @bank = Bank.new(bank_params)
     respond_to do |format|
-        if @bank.save
-          format.html {redirect_to @bank}
-        else
-          format.html { render :new, status: :unprocessable_entity }
-        end
+      if @bank.save 
+        format.html {redirect_to @bank}
+      else
+        format.html { render :new, status: :unprocessable_entity }
       end
+    end
   end
 
   def edit
     @bank = Bank.find(params[:id])
   end
 
-
-
   def update
     @bank = Bank.find(params[:id])
     if @bank.update(bank_params)
       redirect_to @bank
     else
-      render :edit,  :unprocessable_entity
+      render :edit, status:  :unprocessable_entity
     end
   end
 
   def destroy
     @bank = Bank.find(params[:id])
     @bank.destroy
-
     redirect_to root_path, status: :see_other
   end
 
