76d260298cdea0099a7e4865031df7873c2c2571
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-02-14 18:56:11 +0530

    remove action

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.htm
   # Defines the root path route ("/")
+  get "download_pdf", to: 'banks#download_pdf'
+   get "clients", to: 'clients#index'
 
   root "articles#index"
 
   resources :articles do
     resources :comments 
-end
+  end
   resources :orders   do
     resources :customers
    end
-
-
-    resources :banks 
+  resources :banks 
 end
