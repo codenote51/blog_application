f1809a5270abec6cf1bdbcacf4423d030a892d4c
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-02-20 11:57:28 +0530

    remove config

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module BlogApplication
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.action_mailer.default_url_options = { host: 'example.com' }
+    config.asset_host = 'http://example.com'
+
+
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
