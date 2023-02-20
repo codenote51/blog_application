9afbc59ab938a91070b23583aae65e77595e616d
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-02-20 11:58:38 +0530

    remove config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,5 @@ Rails.application.config.assets.version = "1.0"
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( main.css )
-Rails.application.config.assets.precompile += %w( columns.js)
+Rails.application.config.assets.precompile += %w( main.css columns.js main.js)
 
