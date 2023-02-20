4d48f9bf6dbf414355a961ae385445be67d8c210
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-02-20 12:01:07 +0530

    remove format

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,9 @@
+class OrderMailer < ApplicationMailer
+  default from: 'nancykushwah228@gmail.com'
+  def new_order_email
+    attachments.inline['NK.jpg'] = File.read('/home/nancy/nancy/images/Nk.jpg')
+    @order = params[:order]
+    @url  = 'http://example.com/login'
+    mail(to: @order.email, subject: 'Welcome to My Awesome Site')
+  end
+end
