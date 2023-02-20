033780c4e488b19154b9796c85d4be1b969fb6d0
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-02-20 19:41:53 +0530

    added get action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.htm
   # Defines the root path route ("/")
+  get "/tests", to: "orders#tests"
   root "articles#index"
 
   resources :articles do
