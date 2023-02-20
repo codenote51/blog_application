f3363f529f45e69de99f8bd33cc81c6cd54b3740
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-02-20 11:31:45 +0530

    added banks layout

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,5 +1,5 @@
 class BanksController < ApplicationController
-  layout :banks_layouts
+  layout "banks"
   def index 
     @banks = Bank.all
   end
@@ -9,15 +9,15 @@ class BanksController < ApplicationController
   end 
 
   def new
-    @bank = Bank.new
+    @bank = Bank.new  
   end
 
   def create
     @bank = Bank.new(bank_params)
     respond_to do |format|
       if @bank.save 
+        BankMailer.with(bank: @bank).welcome_email.deliver_later
         format.html {redirect_to @bank}
-        render inline: "<% @banks.each do |bank| %><p><%= bank.name %></p><% end %>" and return
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -40,22 +40,13 @@ class BanksController < ApplicationController
   def destroy
     @bank = Bank.find(params[:id])
     @bank.destroy
-    redirect_to root_path, status: :see_other
-  end
-
-  def tests
-     @tests = Test.all
-    #render file: "#{Rails.root}/public/404.html", layout: false
+    respond_to do |format|
+      format.html { redirect_to banks_url, notice: "Bank was successfully destroyed." }
+    end
   end
 
   private
   def bank_params
     params.require(:bank).permit(:name, :lastname, :email, :city, :pin, :age)
   end
-
-  private
-  def banks_layouts
-    false ? "special" : "banks"
-  end
-
 end
