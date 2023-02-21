87578c30e723953a712bcd5312818b63d04611ba
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-02-21 17:46:49 +0530

    remove record

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,4 @@
 class Article < ApplicationRecord
-  include Visible
-  has_many :comments, dependent: :destroy 
-  validates_associated :comments
   validates :title, presence: true
-  validates :body, presence: true, length: { minimum: 10 }
+  validates :body, presence: true
 end
- 
