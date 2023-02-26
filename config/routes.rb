2d24856ec74c7488bc3243be51d1f406d6698d4f
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-02-26 08:43:28 +0530

    [Practice] Table route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.htm
   # Defines the root path route ("/")
   get "/tests", to: "orders#tests"
+  get "/tables-for-n-upto-m/:n/:m", to: "articles#tables"
+  get "/tables-n-upto-m", to: "articles#tables2"
   root "articles#index"
 
   resources :articles do
