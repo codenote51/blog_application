d4ea579c8e02ed489252f8b8d8e4afd91cb5f44b
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-02-27 10:30:47 +0530

    added status column

--- db/migrate/20230130072444_order.rb
+++ db/migrate/20230130072444_order.rb
@@ -1,9 +1,10 @@
 class Order < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
-      t.string :name
-      t.text :lastname
-      t.text :email
+      t.string   :name
+      t.string   :lastname
+      t.string   :status, limit: 15
+      t.string   :email
       
       t.timestamps
     end
