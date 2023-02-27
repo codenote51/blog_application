c7d4c7ccf1361c0ac11fe6e26e63725fcd4ceb09
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-02-27 19:06:30 +0530

    remove record

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_21_060710) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_125521) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "status", limit: 15
@@ -29,7 +29,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_21_060710) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "age"
-    t.integer "#<ActiveRecord::ConnectionAdapters::SQLite3::TableDefinition:0x000055972a77c4c0>"
+    t.integer "#<ActiveRecord::ConnectionAdapters::SQLite3::TableDefinition:0x000055604c220bf8>"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.string "status", limit: 15
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -41,4 +51,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_21_060710) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
