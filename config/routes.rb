55cbd1eb319a273608e18c46dde1d9156b8760d1
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-02-27 12:01:17 +0530

    remove action

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,18 @@ Rails.application.routes.draw do
   get "/tests", to: "orders#tests"
   get "/tables-for-n-upto-m/:n/:m", to: "articles#tables"
   get "/tables-n-upto-m", to: "articles#tables2"
+  #get 'banks', action: :new, controller: 'banks'
 
 
   root "articles#index"
 
-  resources :articles do
-    resources :comments 
-  end
-  resources :orders   do
-    resources :customers
-  end
-  resources :banks
+    resources :articles do
+      resources :comments 
+    end
+    resources :orders   do
+      resources :customers
+    end
+    scope shallow_prefix: "sekret" do
+      resources :banks
+    end
 end
