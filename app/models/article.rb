d3012ae535819db8820b25bc4f0e8131aecc12bb
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-02-28 10:47:07 +0530

    added  validates_associated

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   has_many :comments
+  validates_associated :comments
   validates :title, presence: true
   validates :body, presence: true
   validates :number, numericality: true
