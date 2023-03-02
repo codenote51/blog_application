b2879e262cc7f39e7a40c8bb74699f990053cd4d
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-03-02 15:09:33 +0530

    added config_action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,18 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
+host = 'example.com' #replace with your own url
+config.action_mailer.default_url_options = { host: host }
+
+# SMTP settings for gmail
+config.action_mailer.smtp_settings = {
+  :address              => "nancy9ku@gmail.com",
+  :port                 => 587,
+  :user_name            => nancy
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
