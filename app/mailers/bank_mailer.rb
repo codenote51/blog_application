b105a427cdaae6ba0adfae18bc4ef89b1786fa52
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-03-02 17:22:40 +0530

    remove layout

--- app/mailers/bank_mailer.rb
+++ app/mailers/bank_mailer.rb
@@ -1,10 +1,10 @@
 class BankMailer < ApplicationMailer
   default from: 'notifications@example.com'
 
-
   def welcome_email
     @bank = params[:bank]
     @url =  'http://example.com/login'
-    mail(to: @bank.email, subject: 'Welcome To My Awesome Site')
+    mail(to: @bank.email, 
+        subject: 'Welcome To My Awesome Site')
   end
 end
