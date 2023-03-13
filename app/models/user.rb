450f09032bc71ca439c49b5bbfd3fda2e260568c
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-03-13 17:25:00 +0530

    added passoword length

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   #before_save { self.email = email.downcase }
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { in: 6..10 }
 end
