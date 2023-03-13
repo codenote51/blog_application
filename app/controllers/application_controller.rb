eaa4dcf449701382dd692589e2803dc5848b57fb
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-03-13 19:20:58 +0530

    comments flash message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_action :require_login
-
-  helper_method :user
+  # before_action :require_login
 
   def user
     @user ||= User.find_by(id: session[:user_id])
   end
 
  private
-  def require_login
-    unless logger 
-      flash[:error] = "You must be logged in to access this section"
-      redirect_to new_login_url 
-    end
-  end
+  # def require_login
+  #   unless logger 
+  #     # flash[:notice] = "You must be logged in to access this section"
+  #     redirect_to new_login_url 
+  #   end
+  # end
 end
