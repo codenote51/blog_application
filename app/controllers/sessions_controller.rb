cc3c4c86dfe26431805d159591981ca21dc9a551
Nancy Kushwah <nancykushwah.ror@gmail.com>
2023-03-13 19:23:32 +0530

    change flash messages & redirect_to action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,24 @@
 class SessionsController < ApplicationController
+
   def new
+
   end
 
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to '/login'
     else
-      flash.now[:error] = 'Invalid email/password combination'
-      redirect_to '/signup'
+      flash[:notice] = 'Invalid email/password combination'
+      redirect_to articles_url
     end
   end
 
   def destroy
     session.delete(:user_id)
     @user = nil
-
-    redirect_to root_path
+    #redirect_to root_path
   end
 end
 
